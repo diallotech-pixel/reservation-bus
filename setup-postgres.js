a72f613c1b0805be8d5ac9c8f22037893e46dab3
diallotech <diallosouleymana80@gmail.com>
2026-01-28 01:55:05 +0000

    Ajouter initialisation automatique de la BD

--- setup-postgres.js
+++ setup-postgres.js
@@ -92,7 +92,7 @@ async function setupDatabase() {
     if (userResult.rows[0].count === 0) {
       await pool.query(`
         INSERT INTO utilisateurs (telephone, mot_de_passe, nom_complet) VALUES
-        ('77123456', 'a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3', 'Jean Dupont')
+        ('77123456', 'a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3', 'Souleymana Diallo')
       `);
       console.log('Utilisateur de test inséré');
     }
